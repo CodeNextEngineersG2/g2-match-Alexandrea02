dfa06bc90c4bdef9f631cc1715e3e53d69610fde
Student <student@Admins-MacBook-Air.local>
2018-04-18 18:01:56 -0400

    First commit

--- script.js
+++ script.js
@@ -33,15 +33,25 @@ var resetButton, musicButton;
 
 /*
  * function loadImages()
- * Called in the preload() function. Loads all images needed for your game
+/ Called in the preload() function. Loads all images needed for your game
  * with the loadImage() function. When testing on your machine, be sure to
  * setup a local test server or the images will not load! Your coach will show
  * you how to do this.
- * Example:
+ */ 
    function loadImages() {
-     myImage = loadImage("assets/img/image.png");
+     backImage = loadImage("assets/img/back.png");
+     transitionImage1 = loadImage("assets/img/transition1.png");
+     transitionImage2 = loadImage("assets/img/transition2.png");
+     transitionImage3 = loadImage("assets/img/transition3.png");
+     sunImage = loadImage("assets/img/sun.png");
+     moonImage = loadImage("assets/img/moon.png");
+     boltImage = loadImage("assets/img/bolt.png");
+     cloudImage = loadImage("assets/img/cloud.png");
+     heartImage = loadImage("assets/img/heart.png");
+     smileyImage = loadImage("assets/img/smiley.png");
+
    }
- */
+ 
 
 
 /*
@@ -50,13 +60,18 @@ var resetButton, musicButton;
  * p5.play function "loadAnimation()". Therefore, this function is called after
  * loadImages(). The loadAnimation() function takes image input in the order
  * you'd like the animation to be played, from the first frame to the last.
- * Example:
+ */ 
+
    function loadAnimations() {
-     myAnimation = loadAnimation(img1, img2, img3, img4);
+     sunAnimation = loadAnimation(backImage, transitionImage1, transitionImage2, transitionImage3, sunImage);
+    moonAnimation = loadAnimation(backImage, transitionImage1, transitionImage2, transitionImage3, moonImage);
+    backAnimation = loadAnimation(backImage, transitionImage1, transitionImage2, transitionImage3, backImage);
+    boltAnimation = loadAnimation(backImage, transitionImage1, transitionImage2, transitionImage3, boltImage);
+    cloudAnimation = loadAnimation(backImage, transitionImage1, transitionImage2, transitionImage3, cloudImage);
+    heartAnimation = loadAnimation(backImage, transitionImage1, transitionImage2, transitionImage3, heartImage);
+    smileyAnimation = loadAnimation(backImage, transitionImage1, transitionImage2, transitionImage3, smileyImage);
    }
- */
-
-
+ 
 /*
  * function loadSounds()
  * Works very similarly to loadImages(), only for music and sound effects.
@@ -69,26 +84,47 @@ var resetButton, musicButton;
  */
 
 
-/*
- * function preload()
- * Called automatically by p5.play. Loads all assets for your game (e.g.,
+function preload() {
+ loadImages();
+ loadAnimations();
+}
+ /* Called automatically by p5.play. Loads all assets for your game (e.g.,
  * images, sounds) before p5 calls setup(), to ensure that the game does not
  * begin running until the assets are loaded and ready. Therefore, this function
  * is essentially a "pre-setup" function. 
  */
 
 
-/*
- * function setup()
- * Called automatically by p5.js when the game begins, but after preload().
+  function setup() {
+  gameScreen=createCanvas(790,370);
+  gameScreen.parent("#game-screen");
+  spriteWidth=120;
+  spriteHeight=168;
+  spriteX=70;
+  spriteY=95;
+  imageArray=[backImage,sunImage,moonImage,boltImage,cloudImage,heartImage,smileyImage,transitionImage1,transitionImage2,transitionImage3];
+  resizeImages();
+  createSprites();
+  spriteArray = [sunSprite1,sunSprite2,moonSprite1,moonSprite2,
+  boltSprite1,boltSprite2,cloudSprite1,cloudSprite2,heartSprite1,heartSprite2,smileySprite1,smileySprite2];
+  addAnimations();
+  shuffle(spriteArray, true);
+  placeSprites();
+  spritesActive=true;
+
+
+}
+ /* Called automatically by p5.js when the game begins, but after preload().
  * Therefore, assets are assumed to have been loaded and ready before this
  * function is called.
  */
 
 
-/*
- * function draw()
- */
+function draw() {
+ background(20,40,80);
+ drawSprites();
+ }
+ 
 
 /*
  * function init()
@@ -112,9 +148,13 @@ var resetButton, musicButton;
  */
 
 
-/*
- * function resizeImages()
- * Resizes all images in imageArray such that each image has a width of
+function resizeImages() {
+  for (var i =0; i<imageArray.length; i++) {
+    imageArray[i].resize(spriteWidth,spriteHeight);
+  }
+}
+
+ /* Resizes all images in imageArray such that each image has a width of
  * spriteWidth and a height of spriteHeight. To resize an image use the
  * resize(width, height) method on the image itself.
  * Example of resizing one image:
@@ -128,35 +168,79 @@ var resetButton, musicButton;
  * through the createSprite(x, y, width, height) p5.play method. For all sprites,
  * x and y parameters should be passed values 0 and 0 (sprites are actually placed
  * in a separate function), while width and height correspond to spriteWidth and
- * spriteHeight.
- * Example:
-   function createSprites() {
-     mySprite = createSprite(0, 0, spriteWidth, spriteHeight);
-   }
  */
+   function createSprites() {
+     sunSprite1 = createSprite(0, 0, spriteWidth, spriteHeight);
+     sunSprite2 = createSprite(0, 0, spriteWidth, spriteHeight);
+     moonSprite1 = createSprite(0, 0, spriteWidth, spriteHeight);
+     moonSprite2 = createSprite(0, 0, spriteWidth, spriteHeight);
+    boltSprite1 = createSprite(0, 0, spriteWidth, spriteHeight);
+    smileySprite1 = createSprite(0, 0, spriteWidth, spriteHeight);
+    cloudSprite1 = createSprite(0, 0, spriteWidth, spriteHeight);
+    heartSprite1 = createSprite(0, 0, spriteWidth, spriteHeight);
+    boltSprite2 = createSprite(0, 0, spriteWidth, spriteHeight);
+    smileySprite2 = createSprite(0, 0, spriteWidth, spriteHeight);
+    cloudSprite2 = createSprite(0, 0, spriteWidth, spriteHeight);
+    heartSprite2 = createSprite(0, 0, spriteWidth, spriteHeight);
+   }
+ 
 
 
-/*
- * function addAnimations()
- * Adds an animation to each sprite in spriteArray. The animations have already
+ function addAnimations(){
+  var animations= [sunAnimation,sunAnimation,moonAnimation,moonAnimation,backAnimation,backAnimation,
+  boltAnimation,boltAnimation,cloudAnimation,cloudAnimation,heartAnimation,heartAnimation,smileyAnimation,smileyAnimation];
+  for(var i=0; i<spriteArray.length; i++){
+    spriteArray[i].addAnimation("flip", animations[i]);
+    spriteArray[i].animation.frameDelay=10;
+    spriteArray[i].animation.looping=false;
+    spriteArray[i].animation.playing=false;
+    activateSprite(spriteArray[i]);
+  }
+ }
+ /* Adds an animation to each sprite in spriteArray. The animations have already
  * been loaded using loadAnimations(), so this function is responsible for
  * actually adding them to the sprites. Additionally, this function initializes
  * each animation's frameDelay, loop, and playing properties. Finally, this
- * function calls activateSprite(s) with each sprite as input.
+ * function calls activateSprite(s) with each sprite as inputś
  */
 
 
-/*
- * function placeSprites()
- * Places all sprites in spriteArray on the game screen, according to any
+ function placeSprites() {
+  for(var i =0; i<spriteArray.length; i++) {
+    spriteArray[i].position.x = spriteX;
+    spriteArray[i].position.y = spriteY;
+    if((i+1)%6 ===0) {
+      spriteX=70;
+      spriteY+=spriteHeight + 10; }
+      else{
+        spriteX += spriteWidth +10;
+      }
+    }
+  }
+ 
+
+ /* Places all sprites in spriteArray on the game screen, according to any
  * pattern you like. For starters, try arranging the sprites in a simple
  * grid-like pattern (e.g., 2x2 if you only have four sprites).
  */
 
 
-/*
- * function activateSprite(s)
- * Activates a sprite by initializing its onMousePressed property to a function.
+ function activateSprite(s) {
+  s.onMousePressed = function() {
+    if(spritesActive==true && s.animation.getFrame() != s.animation.getLastFrame()){
+      if(firstChoice==undefined){
+        firstChoice= s;
+        s.animation.goToFrame(s.animation.getLastFrame());
+      }
+      else if(FirstChoice !== s) {
+        secondChoice = s;
+        s.animation.goToFrame(s.animation.getLastFrame());
+      }
+    }
+  }
+ }
+ 
+ /* Activates a sprite by initializing its onMousePressed property to a function.
  * This will essentially cause the sprite to "come alive" and behave like a
  * real playing card when it is clicked.
  * To initialize the onMousePressed property as a function, use a function
